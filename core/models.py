dabb5a9c282f545d0574ce6f43aa086c65a05a4b
Junior Vidotti <jrvidotti@gmail.com>
2014-01-22 18:16:34 -0300

    teste

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Pais(models.Model):
+    sigla = models.CharField(max_length=2, unique=True)
+    nome = models.CharField(max_length=100, unique=True)
+
+    def __unicode__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = u'País'
+        verbose_name_plural = u'Países'
+
+    def estado_relation(self):
+        return self.estado_set.filter(is_present=True)
